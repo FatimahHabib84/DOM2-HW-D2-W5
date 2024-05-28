2002f158f0d47f0ad94fc9ec972c47f771348158
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-05-28 15:16:43 +0300

    edition 2

--- main.js
+++ main.js
@@ -20,5 +20,37 @@ logo.addEventListener('click',()=>{
     cat.style.display = "block"
     roverActionShot = document.getElementsByClassName('rover-action-shot')[0]
     roverActionShot.style.backgroundImage = `url("https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`
+    h11 = document.querySelector('.rover-hero-container>h1')
+    h11.innerText = "We're the Cat People"
+    p1 = document.querySelector('.rover-hero-container>p')
+    p1.innerText = "Book trusted, local pet sitters and walkers who will care for your cat like you would."
+    h31 = document.querySelector(".rover-fact-indiv-1>h3")
+    h31.innerText="Cat Boarding"
+
+    h32 = document.querySelector(".rover-fact-indiv-3>h3")
+    h32.innerText="Cat walking"
+
+    p2 = document.querySelector(".rover-fact-indiv-3>p")
+    p2.innerText="Whenever your cat needs a walk."
+
+
+    h33 = document.querySelector(".rover-fact-indiv-4>h3")
+    h33.innerText="Catty Day Care"
+
+    p3= document.querySelector(".rover-fact-indiv-4>p")
+    p3.innerText="Daytime pet care in your sitter’s cat-friendly home."
+
+    p4 = document.querySelector(".dogy")
+    p4.innerText="cat"
+    
+    p4 = document.getElementsByClassName("dogy2")[0]
+    console.log(p4);
+    p4.innerText="Cat"
+    h2 = document.querySelector(".rover-123-container>h2")
+    h2.innerText = "We’re the treat-your-cat-like-family cat people"
+
+    ttl = document.getElementsByTagName('title')[0]
+    ttl.innerHTML = "Rover.com: Book Cat Boarding Services"
+    
 
 })
